46994e94939ac1ecf1945bf8a8ef6b68b6f582c6
Niko7965 <54095558+Niko7965@users.noreply.github.com>
2019-10-31 12:37:02 +0100

    Getting started

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
+import processing.core.PApplet
 
-public class Main {
+public class Main extends PApplet {
 
     public static void main(String[] args) {
 	// write your code here
+        PApplet.main("com.company.Main")
         System.out.println("HelloWorld");
+    }
+    public void settings(){
+        //Put stuff like size() here
+    }
+    public void setup(){
+        //Ordinary setup
+    }
+    public void draw(){
+        //Ordinary draw
     }
 }
